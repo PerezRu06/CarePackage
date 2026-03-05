c2bf6f68b7d9bce15e33764270bc114bc09b5698
POHMANN001 <ohmann@cs.wisc.edu>
2026-03-05 09:19:53 -0600

    Update front-end code in Driver
    Now runnable!

--- src/Driver.java
+++ src/Driver.java
@@ -3,7 +3,11 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO: our program should do something!
+		LongTermController theControl = new LongTermController();
+		boolean login = theControl.login("pohmann", "secure");
+		boolean added = theControl.addPatient("Dr. Ohmann", "Peter");
+		System.out.println("Logged in? " + login);
+		System.out.println("Patient added? " + added);
 	}
 
 }
